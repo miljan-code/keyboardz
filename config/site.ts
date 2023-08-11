abb47327df382ae5913850c73090cee1bbd32bbf
Miljan <miljancode@gmail.com>
2023-08-11 13:37:42 +0200

    feat: update metadata

--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,15 @@
 import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
+  name: "Keyboardz",
+  description: "Typing test competition",
+  url:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://keyboardz.miljan.xyz",
+  links: {
+    github: "https://github.com/miljan-code/keyboardz",
+  },
   nav: [
     {
       title: "Leaderboard",
