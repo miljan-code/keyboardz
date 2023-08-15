5a44d76420dd749379d1c4561776e978cdf0e815
Miljan <miljancode@gmail.com>
2023-08-15 17:25:10 +0200

    fix: added nextauth secret into env

--- lib/env.ts
+++ lib/env.ts
@@ -2,6 +2,7 @@ import * as z from "zod";
 
 const envVariables = z.object({
   DATABASE_URL: z.string(),
+  NEXTAUTH_SECRET: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
 });
