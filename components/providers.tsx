8e3e3a2a4594e53af277844762b034072e21615b
Miljan <miljancode@gmail.com>
2023-08-23 12:00:59 +0200

    feat(deps): added tanstack query

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as JotaiProvider } from "jotai";
 import { SessionProvider } from "next-auth/react";
 
@@ -7,10 +8,16 @@ interface ProvidersProps {
   children: React.ReactNode;
 }
 
+const queryClient = new QueryClient();
+
 export const Providers = ({ children }: ProvidersProps) => {
   return (
     <SessionProvider>
-      <JotaiProvider>{children}</JotaiProvider>
+      <JotaiProvider>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </JotaiProvider>
     </SessionProvider>
   );
 };
