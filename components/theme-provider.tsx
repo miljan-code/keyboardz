adc05fc9f9fa461a0038dde23790b8a3ab82ebc4
Miljan <miljancode@gmail.com>
2023-08-23 16:09:29 +0200

    feat: change themes

---
+++ components/theme-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ThemeProvider as NextThemeProvider } from "next-themes";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  return (
+    <NextThemeProvider defaultTheme="dark-blue">{children}</NextThemeProvider>
+  );
+};
